2eb8f28eb177a1c60f76db1d0e0bd03eabb5cc4a
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-01-16 10:57:13 +0200

    init

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+command=gcc
+flags=("-Wall" "-Werror" "-Wextra" "-pedantic" "-std=gnu89" "-ggdb")
+files=(./*.c)
+out_dir="./build"
+exec_name="monty"
+
+if [ ! -d "$out_dir" ]; then
+	mkdir "$out_dir"
+fi
+
+betty_doc=$(betty-doc ./*.c)
+if [ $? -ne 0 ]; then
+	echo "$betty_doc"
+fi
+
+betty_style=$(betty-style -q --no-summary ./*.c)
+if [ $? -ne 0 ]; then
+	echo "$betty_style"
+fi
+
+"$command" "${flags[@]}" "${files[@]}" -o "$out_dir"/"$exec_name"
